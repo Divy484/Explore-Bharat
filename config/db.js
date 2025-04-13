2ba216a0aa4023001cf1112f463b5c97f829e4a5
Divy484 <divy1544@gmail.com>
2025-04-13 15:22:17 +0530

    Explore Bharat

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 
 const MySQLConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: 'explore_bharat'
 });
 
