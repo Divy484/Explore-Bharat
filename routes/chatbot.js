f6543868f3915ad36ee1594b1cb0a1a206a42c9d
Divy484 <divy1544@gmail.com>
2026-01-10 13:09:19 +0530

    update gemini version

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -10,7 +10,7 @@ global.Response = fetch.Response;
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
 
 // Initialize conversation history
 let conversationHistory = [];
